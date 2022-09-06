e0de21970ac7e65dc70f270491fb7ee119ff232f
GregT <taylor.gregory2@gmail.com>
2022-09-05 22:53:54 -0600

    mdb removed

---
+++ client/src/Login.js
@@ -0,0 +1,117 @@
+import React, { useEffect } from "react"
+import {useState} from "react"
+
+
+function Login() {
+
+    const [authMode, setAuthMode] = useState("signin")
+
+
+    const changeAuthMode = () => {
+      setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+
+      if (authMode === "signin") {
+        return (
+          <div className="Auth-form-container">
+            <form className="Auth-form">
+              <div className="Auth-form-content">
+                <h3 className="Auth-form-title">Sign In</h3>
+                <div className="text-center">
+                  Not registered yet?{" "}
+                  <span className="link-primary" onClick={changeAuthMode}>
+                    <a href="#">Sign Up</a>
+                  </span>
+                </div>
+                <div className="form-group mt-3">
+                  <label>Username</label>
+                  <input
+                    type="text"
+                    className="form-control mt-1"
+                    placeholder="Enter username"
+                  />
+                </div>
+                <div className="form-group mt-3">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    className="form-control mt-1"
+                    placeholder="Enter password"
+                  />
+                </div>
+                <div className="d-grid gap-2 mt-3">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+                <p className="text-center mt-2">
+                  Forgot <a href="#">password?</a>
+                </p>
+              </div>
+            </form>
+          </div>
+        )
+      }
+
+
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign Up</h3>
+          <div className="form-group mt-3">
+            <label>First Name</label>
+            <input
+            required
+              type="text"
+              className="form-control mt-1"
+              placeholder="First Name"
+            />
+               <label>Last Name</label>
+            <input
+            required
+              type="text"
+              className="form-control mt-1"
+              placeholder="Last Name"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Username</label>
+            <input
+            required
+              type="text"
+              className="form-control mt-1"
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+            required
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Shoe Size</label>
+            <input
+              type="number"
+              min={4}
+              max={15}
+              className="form-control mt-1"
+              placeholder="Enter Shoe Size"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login 
