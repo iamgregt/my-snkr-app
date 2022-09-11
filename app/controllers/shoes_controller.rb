2f43093551a89542544cba10f2847a60af8632d1
GregT <taylor.gregory2@gmail.com>
2022-09-10 23:01:28 -0600

    shoe controller and serializer updated

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -34,7 +34,7 @@ class ShoesController < ApplicationController
     private
 
     def shoe_params
-        params.permit(:brand, :size, :image, :user_id)
+        params.permit(:brand, :size, :image, :user_id, :firebase)
     end
 end
 
--- app/serializers/shoe_serializer.rb
+++ app/serializers/shoe_serializer.rb
@@ -1,3 +1,3 @@
 class ShoeSerializer < ActiveModel::Serializer
-  attributes :id, :size, :image, :user_id
+  attributes :id, :size, :image, :user_id, :firebase
 end
